709a35d55afa817655337ddabc36c9e209adf5cd
kishansinghverma <kishansinghverma@gmail.com>
2020-10-05 19:39:21 +0530

    init

--- databaseManager.js
+++ databaseManager.js
@@ -2,7 +2,7 @@ const MongoClient=require('mongodb').MongoClient;
 const url='mongodb://127.0.0.1:27017/Potato_Manager';
 const params={useUnifiedTopology: true};
 
-const getList=(async function(collectionName, query) {
+const getList=async (collectionName, query)=> {
     let client;
     let data;
 
@@ -16,11 +16,39 @@ const getList=(async function(collectionName, query) {
 
     client.close();
     return data;
-});
+};
+
+const insertOne= async (collectionName, document)=>{
+    let client;
+    let res;
+
+    try {
+        client = await MongoClient.connect(url, params);
+        const db = client.db();
+        const collection = db.collection(collectionName);
+        res = await collection.insertOne(document);
+
+    } catch (err) {console.log(err.stack);}
+
+    client.close();
+    return res;
+
+}
 
 async function getColdStoreNames(){
     const data=await getList('cold_store', {});
     return data;
 }
 
-module.exports={getColdStoreNames};
+async function insertColdStoreName(name, bag, due){
+    const document={name:name, bag:bag, due:due};
+    const res=await insertOne('cold_store', document);
+    if(res != undefined)
+        return (res.insertedCount===1)
+    return false;
+}
+
+module.exports={
+    getColdStoreNames,
+    insertColdStoreName
+};
--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 var db=require('./databaseManager');
+const mustacheExpress=require('mustache-express');
 
 (async ()=>{
-    console.log(await db.getColdStoreNames());
+    const data=await db.insertColdStoreName('Maa Bhadrakali Cold Store', 0, 0);
+    console.log(data);
 })();
+
